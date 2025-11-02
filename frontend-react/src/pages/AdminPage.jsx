f124cb055215cbfe1ec7bf12b009a925fe839ab6
Aditya Pawar <149230860+AdityaByte@users.noreply.github.com>
2025-11-02 21:11:25 +0530

    Pass authToken to DoctorSignup component
    Added authentication token to DoctorSignup component.

--- frontend-react/src/pages/AdminPage.jsx
+++ frontend-react/src/pages/AdminPage.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import DoctorSignup from "./signup_pages/DoctorSignupPage";
 
 const AdminPage = () => {
+
+  const navigate = useNavigate();
+  const authToken = localStorage.getItem("token");
+
+  // This will run when the page loaded to the DOM.
+  useEffect(() => {
+    // If the token not found redirecting to the login page.
+    if (!authToken) {
+      navigate("/login");
+      return;
+    }
+  }, []);
+  
   return (
     <div>
-      <DoctorSignup />
+      <DoctorSignup authToken={authToken}/>
     </div>
   );
 }
