6567846786ac45ba335d2ab294b2a1b01c00b43d
AdityaByte <adityapawar4914@gmail.com>
2025-11-07 19:36:27 +0530

    chore(test): added test dependency and resolved test bug

--- backend-spring-boot/src/test/java/in/ayush/swasthyapath/SwasthyaPathApplicationTest.java
+++ backend-spring-boot/src/test/java/in/ayush/swasthyapath/SwasthyaPathApplicationTest.java
@@ -1,38 +1,15 @@
 package in.ayush.swasthyapath;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+@SpringBootTest
+public class SwasthyaPathApplicationTest {
+
+    @Test
+    void contextLoad() {
+        // Working.
+        assert 1 + 1 == 2;
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
